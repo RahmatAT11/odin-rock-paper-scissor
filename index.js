7fef2b6cbe48f0f2c4424c73a4cc8b1090c83c08
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-07-30 09:32:29 +0700

    Add getComputerChoice function
    This function will generate the computer's choice
    in rock paper scissors game

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Hello, World!");
+
+// this function will generate rock/paper/scissors values
+function getComputerChoice() {
+    //generate a random number from 1 to 3
+    let choiceNumber = Math.ceil(Math.random() * 3);
+    // if the number is one, return rock
+    // if the number is two, return paper
+    // if the number is three, return scissors
+    if (choiceNumber === 1) {
+        return "rock";
+    } else if (choiceNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
