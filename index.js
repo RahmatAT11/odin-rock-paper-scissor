4f5f59e89c63f018885733767b0fca1efefc86a8
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-07-30 09:46:39 +0700

    Add getHumanChoice function
    This function will ask the user to input rock, paper
    or scissors.
    
    If they are inputting something else, throw an error

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+// this function will get an input of rock, paper, scissors
+function getHumanChoice() {
+    // get input from user with prompt and
+    // make the input lower case
+    let userInput = prompt("Enter your choice (rock/paper/scissors):").toLowerCase();
+    // if the input doesn't match any rock paper scissors
+    // choice, then tell the user
+    // if matches, then return the value
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        return userInput;
+    }
+    
+    throw new Error("Wrong choice, please input the right choice!");
+}
